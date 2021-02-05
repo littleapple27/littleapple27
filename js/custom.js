fbab7bcecb483919f15efb8d374340dc7784c425
Andrea Marie <andreamarie@Andreas-MacBook-Pro.local>
2021-02-05 12:26:48 -0500

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,36 @@
-//Get the button
-var scrollButton = document.getElementById("scrollButton");
+$(function () {
+  // Navbar Collapse
+  $(".navbar-collapse a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollButton.style.display = "block";
-  } else {
-    scrollButton.style.display = "none";
+  //ReCaptcha
+  function onSubmit(token) {
+    document.getElementById("contact-form").submit();
   }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  //Get the button
+  var scrollButton = document.getElementById("scrollButton");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+});
